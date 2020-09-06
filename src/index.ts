b3d79a5b934dab97a77d7f627bb6706268ccb94e
Dmitriy Mishutin <dmitriy.mishutin@cxense.com>
2020-09-06 11:42:11 +0400

    Export classes

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
-  }
-  return a + b;
-};
+import NTree from './tree/n-tree';
+import TreeNode from './tree/tree-node';
+
+export * from './types';
+
+export { NTree, TreeNode };
